cff7ff18872f942cdbd1e6576e754928350ddc4a
JuiP <juipradhan2k@gmail.com>
2021-04-04 10:46:47 +0530

    Key changed and mysql-connector installed

--- OrphanageDBMS/settings.py
+++ OrphanageDBMS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w43n86ipnms70)*^*5p2jzq1pkrc+q8r&z&^dvi8u7sj$98a@z'
+SECRET_KEY = 'w43n86ipnms70)*^*5p2jzq1jrah+q8r&z&^dvi8u7sj$98a@z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
